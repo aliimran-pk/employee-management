252a9f9cddc231afa0ca4bb96deef932825e3a3f
Ali Imran <35903658+aliimran-pk@users.noreply.github.com>
2022-02-24 15:54:29 +0500

    Update EmployeeController.java

--- src/main/java/com/ibm/employeemanagment/controllers/EmployeeController.java
+++ src/main/java/com/ibm/employeemanagment/controllers/EmployeeController.java
@@ -23,7 +23,7 @@ public class EmployeeController {
 		emp.setName("Ali Imran");
         emp.setDesignation("Sr. Consulatnt@IBM");
 		emp.setEmpId("emp-"+ random.nextInt(1000));
-		emp.setSalary(9000);
+		emp.setSalary(11000);
 		emp.setDob(new Date());
 		
 		InetAddress addr;
